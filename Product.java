fe080d64df1ce2803074d0eebcdc2864aab23fc0
chximaa <elmehdaouichaimaa@gmail.com>
2025-11-16 21:48:56 +0100

    Update Product.java

--- Product.java
+++ Product.java
@@ -5,24 +5,24 @@ public class Product {
     private String category;
     private int quantity;
     private double price;
+    private Supplier supplier;
 
-    // Constructor
-    public Product(int id, String name, String category, int quantity, double price) {
+    public Product(int id, String name, String category, int quantity, double price, Supplier supplier) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.quantity = quantity;
         this.price = price;
+        this.supplier = supplier;
     }
 
-    // Getters
     public int getId() { return id; }
     public String getName() { return name; }
     public String getCategory() { return category; }
     public int getQuantity() { return quantity; }
     public double getPrice() { return price; }
+    public Supplier getSupplier() { return supplier; }
 
-    // Setters
     public void setQuantity(int quantity) { this.quantity = quantity; }
     public void setPrice(double price) { this.price = price; }
 
